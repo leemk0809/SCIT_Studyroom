4cd5aac57732f5b212ae33c3db05bc73d70b9f36
leemk0809 <qkqhsj34@naver.com>
2022-11-16 08:44:44 +0900

    first, 민국

---
+++ scit-studyroom/src/main/java/net/scit/studyroom/ScitStudyroomApplication.java
@@ -0,0 +1,13 @@
+package net.scit.studyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScitStudyroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScitStudyroomApplication.class, args);
+	}
+
+}
---
+++ scit-studyroom/src/test/java/net/scit/studyroom/ScitStudyroomApplicationTests.java
@@ -0,0 +1,13 @@
+package net.scit.studyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScitStudyroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
